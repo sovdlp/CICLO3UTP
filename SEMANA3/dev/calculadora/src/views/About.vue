1390fbddf430ecd650100b5d37d9922c0057bf06
sovdlp <88692839+sovdlp@users.noreply.github.com>
2021-09-23 19:30:45 -0500

    aboutpage

--- SEMANA3/dev/calculadora/src/views/About.vue
+++ SEMANA3/dev/calculadora/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <calculadora-basica />
   </div>
 </template>
+
+<script>
+import CalculadoraBasica from '../components/CalculadoraBasica.vue'
+
+export default ({
+  name: 'About',
+  components: {
+    CalculadoraBasica
+  },
+})
+</script>
+
