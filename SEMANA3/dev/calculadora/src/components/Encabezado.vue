2243d2f9a6f274905cd7d260881225915bcd1e25
sovdlp <88692839+sovdlp@users.noreply.github.com>
2021-09-23 20:33:14 -0500

    23-09-21

--- SEMANA3/dev/calculadora/src/components/Encabezado.vue
+++ SEMANA3/dev/calculadora/src/components/Encabezado.vue
@@ -2,8 +2,6 @@
   <div>
       <p align="center" class="text-uppercase">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, quaerat? Eligendi neque iusto, minus maxime perferendis ipsum, unde consequuntur corrupti quod dolor totam amet optio? Velit tempora reprehenderit voluptates distinctio.
-          <br />
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi similique, doloribus quibusdam consequuntur sed, iusto laudantium debitis officiis dicta rerum exercitationem repellendus perspiciatis doloremque nobis facilis dolores tenetur obcaecati? Tempora!
       </p>
   </div>
 </template>
---
+++ SEMANA3/dev/calculadora/src/views/ListarPersonajes.vue
@@ -0,0 +1,15 @@
+<template>
+  <personajes />
+</template>
+
+<script>
+import Personajes from '../components/Personajes.vue'
+export default {
+  components: { Personajes },
+
+}
+</script>
+
+<style>
+
+</style>
