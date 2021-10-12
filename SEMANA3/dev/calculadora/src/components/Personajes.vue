0fc589f6376feadcebee8a6aca120f26c71c4176
sovdlp <88692839+sovdlp@users.noreply.github.com>
2021-10-11 21:31:26 -0500

    Merge branch 'main' of https://github.com/sovdlp/CICLO3UTP into main

--- SEMANA3/dev/calculadora/src/components/Personajes.vue
+++ SEMANA3/dev/calculadora/src/components/Personajes.vue
@@ -1,12 +1,50 @@
 <template>
   <div>
-    <h1>Personajes</h1>
-    <ul>
-      <li v-for="item in personajes" :key="item.id"> <!--item.name-->
-          {{item.nombre}} {{item.apellido}} - Edad:{{item.edad}}
-          <!--{{ item.Name }} - {{ item.Age }}-->
-      </li>
-    </ul>
+    <h1>MEJORES DELANTEROS DEL FUTBOL INTERNACIONAL</h1>
+    <!-- inicio tabla de delanteros-->
+    <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-center">
+            NOMBRE
+          </th>
+          <th class="text-center">
+            APELLIDO
+          </th>
+          <th class="text-center">
+            EDAD
+          </th>
+          <th class="text-center">
+            EMAIL
+          </th>
+          <th class="text-center">
+            ACCIONES
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in personajes"
+          :key="item.name"
+        >
+          <td>{{ item.nombre }}</td>
+          <td>{{ item.apellido }}</td>
+          <td>{{ item.edad }}</td>
+          <td>{{ item.email }}</td>
+          <td>
+            <v-btn
+              color="accent"
+              levation="12"
+              rounded
+              x-small
+            >ELIMINAR</v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+  <!-- fin tabla de jugadores --> 
   </div>
 </template>
 
